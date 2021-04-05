0b66cb9dd9c125012ef4aab78133d353090b3894
CL <77945903+chenlee238@users.noreply.github.com>
2021-04-05 07:48:20 -0400

    Fix slide image

--- main.js
+++ main.js
@@ -4,26 +4,18 @@ showSlides(slideIndex);
 
 function plusSlides(n){
     showSlides(slideIndex += n);
-    // if(n < 3) { n = 1};
 }
 
 function showSlides(n){
     var slides = document.getElementsByClassName('producer');
-    var btnNext = document.querySelector('.next');
-    
 
-    if(n > slides.length) {slidesIndex = 1};
-    if(n < 1) {slideIndex = slides.length; console.log(slides.length)};
-    if(n > 4){
-        n = 1;
-    }
+    if(n > slides.length) {slideIndex = 1};
+    if(n < 1) {slideIndex = slides.length};
 
     for( let i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
     }
-
     slides[slideIndex-1].style.display = "flex";
-    // console.log(n);
 }
 
 
